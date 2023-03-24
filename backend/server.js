716b88074abd20a69d24dddd8eb3b29a1977af4b
Offitt <offitt90@gmail.com>
2023-03-23 23:24:11 -0400

    Initial Commit

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const express = require('express');
+const colors = require('colors');
+const dotenv = require('dotenv').config();
+const PORT = process.env.PORT || 5000;
+
+// Connect to database
+// connectDB()
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Routes
+
+// Serve Frontend
+if (process.env.NODE_ENV === 'production') {
+  // Set build folder as static
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+  // FIX: below code fixes app crashing on refresh in deployment
+  app.get('*', (_, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
+  });
+} else {
+  app.get('/', (req, res) => {
+    res.status(200).json({ message: 'Welcome to the Support Desk API' });
+  });
+}
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
