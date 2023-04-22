35026845aca53648fad1776fee0a9df379c90ec9
Offitt <offitt90@gmail.com>
2023-04-22 06:21:03 -0400

    added shipping page

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -1,11 +1,25 @@
-import mongoose from 'mongoose';
+import mongoose from 'mongoose'
 
 const orderSchema = mongoose.Schema(
   {
     user: {
       type: mongoose.Schema.Types.ObjectId,
-      required: true,
+      required: false,
       ref: 'User',
+    },
+    nonRegUser: {
+      firstName: {
+        type: String,
+      },
+      lastName: {
+        type: String,
+      },
+      email: {
+        type: String,
+      },
+      phoneNumber: {
+        type: Number,
+      },
     },
     orderItems: [
       {
@@ -71,8 +85,8 @@ const orderSchema = mongoose.Schema(
   {
     timestamps: true,
   }
-);
+)
 
-const Order = mongoose.model('Order', orderSchema);
+const Order = mongoose.model('Order', orderSchema)
 
-export default Order;
+export default Order
---
+++ frontend/src/pages/Shipping.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Shipping = () => {
+  return (
+    <div>
+      <div className='container mx-auto px-4 md:px-0'>
+        <h1 className='text-3xl font-bold text-center my-4 md:text-left'>
+          SHOPPING CART
+        </h1>
+      </div>
+    </div>
+  )
+}
+
+export default Shipping
