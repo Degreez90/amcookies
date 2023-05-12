9516e97996930cae57afcbc4860e788041ded949
Offitt <offitt90@gmail.com>
2023-05-11 21:39:27 -0400

    Changed path

--- backend/server.js
+++ backend/server.js
@@ -30,12 +30,12 @@ const __dirname = path.resolve()
 
 if (process.env.NODE_ENV === 'production') {
   // Set build folder as static
-  app.use(express.static(path.join(__dirname, '../frontend/dist')))
+  app.use(express.static(path.join(__dirname, '/frontend/dist')))
 
   // FIX: below code fixes app crashing on refresh in deployment
   app.get('*', (_, res) => {
     // res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'))
-    res.sendFile(path.join(__dirname, '../frontend/dist/index.html'))
+    res.sendFile(path.join(__dirname, 'frontend/dist/index.html'))
   })
 } else {
   app.get('/', (req, res) => {
