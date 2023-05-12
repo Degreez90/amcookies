00bcb61bc90e07d0fbc857bcc7fa459858e697c6
Offitt <offitt90@gmail.com>
2023-05-12 12:26:53 -0400

    changed server

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,7 @@ app.get('/api/config/paypal', (req, res) =>
 const __dirname = path.resolve()
 // app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'development') {
   // Set build folder as static
   app.use(express.static(path.join(__dirname, '/frontend/dist')))
 
