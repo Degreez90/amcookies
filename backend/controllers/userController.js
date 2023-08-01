4feecae97588ccadcfd9d23a0697d8b5559260c1
Mark Offitt <offitt@gmail.com>
2023-07-31 20:04:20 -0400

    Changes to userModel

---
+++ backend/controllers/userController.js
@@ -0,0 +1,41 @@
+const asyncHandler = require('express-async-handler')
+
+const User = require('../models/userModel')
+
+const registerUser = asyncHandler(async (req, res) => {
+  const { firstName, lastName, email, password } = req.body
+
+  // Validation
+  if (!firstName || !lastName || !email || !password) {
+    res.status(400)
+    throw new Error('Please include all fields')
+  }
+
+  // Find if user already exists
+  const userExists = await User.findOne({ email })
+
+  if (userExists) {
+    res.status(400)
+    throw new Error('User already exists')
+  }
+
+  // Create user
+  const user = await User.create({
+    firstName,
+    lastName,
+    email,
+    password,
+  })
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id),
+    })
+  } else {
+    res.status(400)
+    throw new error('Invalid user data')
+  }
+})
